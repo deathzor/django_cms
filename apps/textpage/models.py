41a4a55548b9bf7e75439b0af8fd511d0e146412
root <root@deathzor.localdomain>
2019-04-08 00:45:28 +0000

    added basic menu functionality

--- apps/textpage/models.py
+++ apps/textpage/models.py
@@ -5,3 +5,8 @@ class textpage(models.Model):
 	page = models.CharField(max_length=100,unique=True)
 	title = models.CharField(max_length=100)
 	html = models.TextField(max_length=30000,default="")
+
+class menuitem(models.Model):
+        title = models.CharField(max_length=100)
+        parent = models.ForeignKey('self',on_delete=models.DO_NOTHING,blank = True,null=True)
+        page = models.ForeignKey(textpage,on_delete=models.DO_NOTHING,blank = True,null=True)
--- apps/textpage/views.py
+++ apps/textpage/views.py
@@ -8,8 +8,12 @@ def pageView(request,page=None):
     if (page == None):
      page = "index"
     page = textpage.objects.get(page=page)
-    print(page.html);
+    #menustructure = menuitem.object.get(parent=None)
+    menuobject = menuitem.objects.filter(parent=None)
+    menu = menuobject.values()
+    for item in menu:
+        item['page'] = textpage.objects.get(id=item['page_id']).page
     template = loader.get_template('textpage/templates/textpage.html')
     title = 'home';
-    context = { 'html': page.html,"title":page.title }
+    context = { 'html': page.html,"title":page.title, 'menu':menu}
     return HttpResponse(template.render(context,request))
